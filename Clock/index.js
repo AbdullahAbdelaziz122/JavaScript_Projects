0939c8333c55560d15cd2465f09fa74461077b68
AbdullahAbdelaziz122 <abdullah.abdelaziz15@gmail.com>
2024-08-17 18:08:59 +0300

    initial commit

---
+++ Clock/index.js
@@ -0,0 +1,11 @@
+function updateClock(){
+    const now = new Date();
+    const hour = now.getHours();
+    const min = now.getMinutes();
+    const sec = now.getSeconds();
+    const timeString = `${hour}:${min}:${sec}`
+    document.getElementById("clock").textContent = timeString;
+}
+updateClock();
+
+setInterval(updateClock, 1000);
